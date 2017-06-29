e0852344e55a0c6d2d978c5bcc8d5e2763f34ffb
Jessie <jessie.huang@lalamove.com>
2017-06-29 17:35:29 +0700

    create Form UI

--- app/index.js
+++ app/index.js
@@ -1,11 +1,18 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
+var Form = require('./components/form.js');
 
 class App extends React.Component {
   render() {
     return (
-      <div>Hello World!</div>
+      <div>
+          <div className="container">
+            <div className="jumbotron">
+              <Form name="test"/>
+            </div>
+          </div>
+      </div>
     )
   }
 }
